f8d69e30181af699ced105223f12594d1b784852
Gregory Richens <gregoryrichens@gmail.com>
2020-04-03 21:10:48 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_list = {}
+  pigeon_data.each do |(attribute, value_hash)|
+    value_hash.each do |value, name_array| 
+      name_array.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+        #do I need to include this in if block?
+        pigeon_list[name][attribute] = []
+      end
+    end
+  end
+  pigeon_list.each do |(name, attribute_hash)|
+    attribute_hash.each do |(attribute, value_array)|
+      pigeon_data.each do |(attribute2, value_hash)|
+        value_hash.each do |value, name_array|
+          name_array.each do |name2|
+            if name2 == name &&  attribute2 == attribute
+              value_array << value.to_s
+            end
+          end
+        end
+      end
+    end
+    pigeon_list
+  end 
 end

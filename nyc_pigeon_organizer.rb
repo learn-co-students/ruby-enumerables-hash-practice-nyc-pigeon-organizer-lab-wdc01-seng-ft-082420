cedaf0e8ecf1ac879676e6fae4477b3f9aa3bde3
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-21 00:30:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,10 +2,8 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(key, value), final_array|
-    #binding.pry
     value.each do |inner_key, names|
       names.each do |name|
-        #binding.pry
         if !final_array[name]
           final_array[name] = {}
         end
@@ -16,5 +14,4 @@ def nyc_pigeon_organizer(data)
       end
     end
    end
-  # binding.pry
 end
